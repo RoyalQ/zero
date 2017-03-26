7112e9d6863a1fa81f29c1892f961842a3222991
RoyalQ <792449184@qq.com>
2017-03-26 20:27:36 +0800

    a

---
+++ 5/5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  5
+//
+//  Created by user on 2017/3/26.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
